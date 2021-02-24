89e30a6b0477f407dca2c39c6f6e2e80434234e8
tester-404 <test404bugs@gmail.com>
2021-02-24 00:29:32 -0800

    comment

---
+++ users.js
@@ -0,0 +1,21 @@
+import supertest from 'supertest';
+const request = supertest('https://gorest.co.in/public-api/');
+
+import { expect } from 'chai';
+
+const TOKEN =
+    '028186c4431de8750ed1a11c0b0ce09cd985530d91eed768de6a137fa4a15882';
+
+describe('Users', () => {
+    it('GET /users', (done) => {
+        //  request
+        //  .get('users?access-token=TOKEN');
+        request.get('users?access-token=${TOKEN}').end((err, res) => {
+         //   console.log(err);
+         //   console.log(res.body);
+         expect(res.body.data).to.not.be.empty;
+     //    expect(res.body.data).to.be.empty;
+         done();
+        });
+    });
+});
